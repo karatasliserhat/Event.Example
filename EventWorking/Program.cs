306d7bb85b8c170cd810a8d6de487545c78772c7
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2025-03-12 15:15:39 +0300

    event çalışma mantığı.....

---
+++ EventWorking/Program.cs
@@ -0,0 +1,41 @@
+﻿
+MyEventPublisher publisher = new();
+//publisher.XEvent += () => Console.WriteLine("Event Raised");
+
+publisher.XEvent += X;
+
+
+void X()
+{
+    Console.WriteLine("Event Tetiklendi.");
+}
+
+publisher.RaiseEvent();
+publisher.XEvent -= X;
+
+publisher.RaiseEvent();
+class MyEventPublisher()
+{
+    public delegate void XHandler();
+
+    XHandler xDelegate;
+    public event XHandler XEvent
+    {
+        add
+        {
+            Console.WriteLine("Event Added");
+            xDelegate += value;
+        }
+        remove
+        {
+            Console.WriteLine("Event Removed");
+            xDelegate -= value;
+        }
+
+    }
+
+    public void RaiseEvent()
+    {
+        xDelegate?.Invoke();
+    }
+}
